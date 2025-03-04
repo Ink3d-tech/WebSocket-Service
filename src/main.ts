bac668235a11bb243c76f9f629cd5db17565a422
Camiloc7 <j.camilocorredor@gmail.com>
2025-03-04 16:52:54 -0500

    cambios en main

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { Server } from 'socket.io';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
+class WebSocketAdapter extends IoAdapter {
+  createIOServer(port: number, options?: any): any {
+    const server = new Server(port, {
+      cors: {
+        origin: process.env.CLIENT_URL || '*',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Authorization'],
+        credentials: true,
+      },
+    });
+    return server;
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: process.env.CLIENT_URL || '*',
-    methods: 'GET,POST,PUT,DELETE,PATCH',
-    allowedHeaders: 'Content-Type,Authorization',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
+  app.useWebSocketAdapter(new WebSocketAdapter(app)); 
 
   const PORT = process.env.PORT || 3001;
   await app.listen(PORT);
